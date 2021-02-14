190ca8c6a8979259f645330a9f7730eacad7f892
Alexander Klanovets <alex.klanovets@gmail.com>
2021-02-14 06:37:40 +0200

    refactor: Small schema refactoring

--- src/server/schemas/movieValidationSchema.js
+++ src/server/schemas/movieValidationSchema.js
@@ -2,9 +2,11 @@
 
 const Joi = require('joi');
 
+const FIRST_MOVIE_RELEASE_YEAR = 1888;
+
 const movieValidationSchema = Joi.object({
   title: Joi.string().max(255).required(),
-  year: Joi.number().integer().min(1888).required(),
+  year: Joi.number().integer().min(FIRST_MOVIE_RELEASE_YEAR).required(),
   format: Joi.string().valid('VHS', 'DVD', 'Blu-Ray').required(),
   actors: Joi.array().items(Joi.string().max(255).required()),
 });
