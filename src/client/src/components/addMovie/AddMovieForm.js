43f05fdd4684e81662e438c91147d8aa1cc3f5b4
Alexander Klanovets <alex.klanovets@gmail.com>
2021-02-16 12:22:36 +0200

    fix: Disallow blanks fields for title and actors on the client

--- src/client/src/components/addMovie/AddMovieForm.js
+++ src/client/src/components/addMovie/AddMovieForm.js
@@ -9,6 +9,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const isValidMovieForm = (movieData) => {
+  const { title, actors } = movieData;
+  if (title.trim() === '') {
+    alert('Please, provide a non-blank title.');
+    return false;
+  }
+  actors.forEach((actor) => {
+    if (actor.length > 255) {
+      alert('The name of each actor must be less than 256 characters long.');
+      return false;
+    }
+  });
+  return true;
+};
+
 export default function AddMovieForm() {
   const [title, setTitle] = React.useState('');
   const [year, setYear] = React.useState(new Date().getFullYear());
@@ -25,9 +40,9 @@ export default function AddMovieForm() {
     const year = parseInt(document.getElementsByName('year')[0].value);
     const format = document.getElementsByName('format')[0].value;
     const actors = document.getElementsByName('actors')[0].value
-      .split(/,\s*/);
+      .split(/, */).map((actor) => actor.trim());
 
-    console.log({ title, year, format, actors });
+    if (!isValidMovieForm({ title, actors })) return;
 
     const res = await fetch('/api/movies', {
       method: 'POST',
@@ -48,18 +63,19 @@ export default function AddMovieForm() {
     <form onSubmit={handleSubmit}>
       <TextField
         id="outlined-basic" label="Title" variant="outlined" fullWidth={true}
-        margin="normal" inputProps={{ maxLength: 255 }} name="title"
-        value={title} required
-        onChange={(event) => handleChange(event, setTitle)}
+        margin="normal" name="title" value={title}
+        inputProps={{ maxLength: 255 }}
+        onChange={(event) => handleChange(event, setTitle)} required
       />
       <TextField
         id="outlined-basic" label="Year" variant="outlined" fullWidth={true}
-        margin="normal" name="year" value={year} type="number" required
-        onChange={(event) => handleChange(event, setYear)}
+        margin="normal" name="year" value={year} type="number"
+        inputProps={{ min:1888, max: new Date().getFullYear() + 30 }}
+        onChange={(event) => handleChange(event, setYear)} required
       />
       <TextField
         id="outlined-basic" label="Format" variant="outlined" fullWidth={true}
-        margin="normal" value={format} name="format" select
+        margin="normal" name="format" value={format} select
         onChange={(event) => handleChange(event, setFormat)}
       >
         <MenuItem key="VHS" value="VHS">VHS</MenuItem>
@@ -67,10 +83,13 @@ export default function AddMovieForm() {
         <MenuItem key="Blu-Ray" value="Blu-Ray">Blu-Ray</MenuItem>
       </TextField>
       <TextField
-        id="outlined-basic" label="Actors" variant="outlined"
-        fullWidth={true} margin="normal" name="actors" 
-        value={actors} required
-        onChange={(event) => handleChange(event, setActors)}
+        id="outlined-basic" label="Actors"
+        variant="outlined" fullWidth={true} margin="normal" name="actors" value={actors}
+        inputProps={{
+          pattern: '(([A-za-z]+( +[A-za-z]+)*)(, *)?)+',
+          title: 'Actors\' names must be comma separated words (or multiple words) containing english letters.',
+        }}
+        onChange={(event) => handleChange(event, setActors)} required
       />
       <Button 
         className={classes.addButton} variant="contained"
