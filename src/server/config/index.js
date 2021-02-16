a4aa556b3514e084f444a29b145a8d91a36dbda6
Alexander Klanovets <alex.klanovets@gmail.com>
2021-02-16 15:24:25 +0200

    fix: Add logger config from .env

--- src/server/config/index.js
+++ src/server/config/index.js
@@ -6,5 +6,6 @@ const { parseIntOrDefault } = require('../utils');
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
+  LOGS_PATH: process.env.LOGS_PATH,
   SHUTDOWN_TIMEOUT: parseIntOrDefault(process.env.SHUTDOWN_TIMEOUT, 5000),
 };
